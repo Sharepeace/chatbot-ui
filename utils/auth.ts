19d13cc78e978819c613d60e8d5d6a70a2fb774f
John kim <jeo1kim@gmail.com>
2023-05-05 02:54:09 -0700

    convert files to ts

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,6 +1,12 @@
-import { supabase } from '@/utils/client'
+import { supabase } from '@/utils/client';
 
-export async function signInWithToken(token) {
+interface UserResponse {
+  user: {
+    id: string;
+  };
+}
+
+export async function signInWithToken(token: string): Promise<void> {
   const response = await fetch('/api/verify-token', {
     method: 'POST',
     headers: {
@@ -10,16 +16,14 @@ export async function signInWithToken(token) {
   });
 
   if (response.ok) {
-    const { user } = await response.json();
+    const { user } = (await response.json()) as UserResponse;
     // Log the user in using Supabase's session management
-    const { session, error } = await supabase.auth.signIn({
-      user: user.id,
-    });
+    const { data, error } = await supabase.auth.getSession()
 
     if (error) {
       console.error('Error logging in:', error.message);
     } else {
-      console.log('User logged in successfully:', session.user);
+      console.log('User logged in successfully:', data?.session?.user);
     }
   } else {
     console.error('Token verification failed:', response.statusText);
